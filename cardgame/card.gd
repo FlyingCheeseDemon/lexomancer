acdaefae23654f204e384c495403f72af2f579db
FlyingCheeseDemon <jakobbuehler@gmx.net>
2026-09-01 13:22:29 +0200

    Actual card descriptions

--- cardgame/card.gd
+++ cardgame/card.gd
@@ -12,23 +12,22 @@ static func from_statement(statement:Statement) -> Card:
 	obj.statement = statement
 	obj.image = obj.statement.data.artwork
 	obj.title = obj.statement.data.title
-	obj.description = format_card_text(obj.statement.data.text,obj.statement.substatement_types)
-
+	obj.description = obj.statement.data.card_text
 	return obj
 
-static func format_card_text(oringial_text:String,substate_types:Array[ENUMS.ST_TYPES]) -> String:
-	var substrings:PackedStringArray = oringial_text.split(" ")
-	var new_text:String = ""
-	var text:String
-	for substring:String in substrings:
-		if substring[0] == "%":
-			var index = int(substring[1])
-			var color = COLORS.ST_COLORS[substate_types[index]]
-			var type_name = ENUMS.ST_TYPES.keys()[substate_types[index]]
-			if type_name == "CONJUNCTION":
-				type_name = "???"
-			text = "[color=" + color + "]"+ type_name +"[/color]"
-		else:
-			text = substring
-		new_text += text + " "
-	return new_text
+#static func format_card_text(oringial_text:String,substate_types:Array[ENUMS.ST_TYPES]) -> String:
+	#var substrings:PackedStringArray = oringial_text.split(" ")
+	#var new_text:String = ""
+	#var text:String
+	#for substring:String in substrings:
+		#if substring[0] == "%":
+			#var index = int(substring[1])
+			#var color = COLORS.ST_COLORS[substate_types[index]]
+			#var type_name = ENUMS.ST_TYPES.keys()[substate_types[index]]
+			#if type_name == "CONJUNCTION":
+				#type_name = "???"
+			#text = "[color=" + color + "]"+ type_name +"[/color]"
+		#else:
+			#text = substring
+		#new_text += text + " "
+	#return new_text
--- scripts/statement_data.gd
+++ scripts/statement_data.gd
@@ -1,11 +1,14 @@
 extends Resource
 class_name StatementData
 
-@export var type:ENUMS.ST_TYPES
 @export var name:String
+
+@export_category("Card visuals")
 @export var title:String
 @export var artwork:Texture2D
+@export_multiline var card_text:String
 
-@export var text:String
-
+@export_category("Function")
+@export var type:ENUMS.ST_TYPES
+@export_multiline var text:String
 @export var substatement_types:Array[ENUMS.ST_TYPES] = []
